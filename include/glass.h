5ca994f3f2607d65c380e9d8ecf6406657812f7b
Devang R Mehta <devangrm@outlook.de>
2020-09-12 01:25:02 +0200

    read and write image

---
+++ include/glass.h
@@ -0,0 +1,17 @@
+#ifndef __GLASS_H__
+#define __GLASS_H__
+
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+
+#include <opencv2/core/core.hpp>
+
+#include"../include/readFile.h"
+#include"../include/rwFile.h"
+
+typedef unsigned int int32;
+typedef short int16;
+typedef unsigned char byte;
+
+#endif // !__GLASS_H__
--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,36 @@
-#include<iostream>
-#include<fstream>
+#include"../include/glass.h"
 
 int main()
 {
-	// read defect free images
-	std::fstream defectFreeImages;
-	defect
-	// read defect mask
-	std::fstream defectMask;
+    byte* pixels_df = NULL, * pixels_m = NULL;
+    int32 width_df, width_m;
+    int32 height_df, height_m;
+    int32 bytesPerPixel_df, bytesPerPixel_m;
 
+	int x_offset = 0;
+	int y_offset = 0;
+
+	//readFile* file = new readFile();
+
+	// read defect free images
+    
+	//file->readImage("../../../images/defectFreeImages/Punkte 0.3.bmp", &pixels_df, &width_df, &height_df, &bytesPerPixel_df);
+	//free(pixels_df);
+	BMP bmp("../../../images/defectFreeImages/Punkte 0.3.bmp");
+
+	// read defect mask
+
+	//file->readImage("../../../images/defectImages/Punkte 0.3 mask.bmp", &pixels_m, &width_m, &height_m, &bytesPerPixel_m);
+	//free(pixels_m);
+	BMP bmp1("../../../images/defectImages/Punkte 0.3 mask.bmp");
+
+	// initialize random seed
+	srand(1);
+
+	//x_offset = rand() % (width_df - width_m);
+	//y_offset = rand() % (height_df - height_m);
+	//std::cout << x_offset << " " << y_offset << std::endl;
+
+	//delete(file);
+	return 0;
 }
